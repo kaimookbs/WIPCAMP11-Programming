074a1e471013b963e5c376c1bd2a390ee2fe1962
kaimookbs <kaimook0804@gmail.com>
2019-05-30 11:38:36 +0700

    @newproject

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,23 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("ชื่อไข่มุก ");
+        
+        System.out.print("รสมันม่วง \n");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.print("เพื่อนชื่อ มิ้น \n");
+        
+        System.out.print("เพื่อนอีกคนชื่อ โฉม");
+        
+    }
+}
